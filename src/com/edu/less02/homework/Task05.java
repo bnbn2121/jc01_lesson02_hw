ae55472e5bbe4c20bbf0791cfdf4146812c79c1e
bnbn2121 <shelutods@gmail.com>
2025-03-03 13:48:17 +0300

    Update Task05.java

--- src/com/edu/less02/homework/Task05.java
+++ src/com/edu/less02/homework/Task05.java
@@ -3,10 +3,10 @@ package com.edu.less02.homework;
 public class Task05 {
 
 	public static void main(String[] args) {
-		double w; 		/* ширина */
-		double l; 		/* длина */
-		double k; 		/* коэф. отношения длины к ширине */
-		double s; 		/* площадь */
+		double w; 		// ширина
+		double l; 		// длина
+		double k; 		// коэф. отношения длины к ширине
+		double s; 		// площадь
 
 		/* Дано */
 		l = 8;
